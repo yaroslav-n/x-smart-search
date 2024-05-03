4d935bb6a2206fea97c01265ab7cb3486917735a
Yaroslav Nazarov <ya.nazarov@gmail.com>
2024-05-03 22:34:32 +1000

    feat: add injection, improve icon

--- src/background/background.ts
+++ src/background/background.ts
@@ -4,3 +4,14 @@ chrome.action.onClicked.addListener(async () => {
         url: chrome.runtime.getURL("assets/settings.html"),
     });
 });
+
+// ----- messages from typebar.app
+chrome.scripting.registerContentScripts([
+    {
+        id: `typebar_isolated_context_inject_${Math.random()}`,
+        world: "ISOLATED",
+        matches: ["https://twitter.com/*", "https://x.com/*", "https://pro.twitter.com/*", "https://pro.x.com/*"],
+        js: ["lib/inject.js"],
+        runAt: "document_start",
+    },
+]);
